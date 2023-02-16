d13b585732405014c0f218c450fd515e8c5c2296
joshua-wade7 <joshua.nelson0124@gmail.com>
2023-02-15 21:28:43 -0700

    added more elements to the html, and updated pseudo.txt with outline of what to do.

--- assets/css/script.js
+++ assets/css/script.js
@@ -1,2 +1,35 @@
 //Variable Declarations
 //Create my object of quiz questions first and then move on to other variables.
+
+var questions = {
+    title1: "When attaching an event listener to a submission form, which event listener would you use to capture the users input?",
+
+    choices1: ["Click", "Hover", "Submit", "Keypress"],
+
+    answer1: "Submit"
+
+    title2:
+
+    choices2:
+
+    answer2:
+
+    title3:
+
+    choices3:
+
+    answer3:
+
+    title4:
+
+    choices4:
+
+    answer4:
+
+    title5:
+
+    choices5:
+
+    answer5:
+
+}
